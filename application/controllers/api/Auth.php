d6b1e32013f28ae31329f82c8c605728c8ad869a
havizIM <viz.ndinq@gmail.com>
2019-05-27 22:24:17 +0700

    Update Haviz

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -287,21 +287,22 @@ class Auth extends CI_Controller {
 
           $send = $this->email->send();
 
-          if (!$send) {
-            json_output(400, array('status' => 400, 'description' => 'Gagal', 'message' => 'Tidak dapat mengirim email'));
-          } else {
-            $data = array(
-              'password' => $new_password
-            );
+          echo $send;
+          // if (!$send) {
+          //   json_output(400, array('status' => 400, 'description' => 'Gagal', 'message' => 'Tidak dapat mengirim email'));
+          // } else {
+          //   $data = array(
+          //     'password' => $new_password
+          //   );
 
-            $update = $this->AuthModel->updateClient($param, $data);
+          //   $update = $this->AuthModel->updateClient($param, $data);
 
-            if(!$update){
-							json_output(400, array('status' => 400, 'description' => 'Failed', 'message' => 'Gagal melakukan reset password' ));
-						} else {
-							json_output(200, array('status' => 200, 'description' => 'Success', 'message' => 'Berhasil melakukan reset password. Silahkan cek email anda untuk mendapatkan password baru'));
-						}
-          }
+          //   if(!$update){
+					// 		json_output(400, array('status' => 400, 'description' => 'Failed', 'message' => 'Gagal melakukan reset password' ));
+					// 	} else {
+					// 		json_output(200, array('status' => 200, 'description' => 'Success', 'message' => 'Berhasil melakukan reset password. Silahkan cek email anda untuk mendapatkan password baru'));
+					// 	}
+          // }
         }
       }
     }
